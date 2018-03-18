5e4fcd6138aa0355df0077900472a9f5a9f19fd3
Joey Kimsey <Joeyk4816@gmail.com>
2018-03-18 19:28:43 -0400

    Initial commit

---
+++ js/background.js
@@ -0,0 +1,133 @@
+/**
+ * Background processor for TempusTools
+ * 
+ * Handles various tasks including:
+ * - adding the outgoing header
+ * - adding a listener for message from devtools to run console log on
+ * - inline script
+ * 
+ * @author Aaron Saray aaron@aaronsaray.com
+ *
+ * Rewritten for specialized application by:
+ * 
+ * @author Joey Kimsey joeyk4816@gmail.com
+ */
+
+/**
+* set up a quick namespace as not to pollute
+*/
+var TempusTools = TempusTools || {};
+
+/**
+ * Define to send headers
+ */
+TempusTools.addHeaders = true;
+
+/**
+ * Max combined header size of 256kb
+ */
+TempusTools.maxCombinedSize = 261120;
+
+/**
+ * Whether the plugin is enabled
+ */
+TempusTools.enabled = true;
+
+/**
+ * Function to show in the browser action and text whether this is enabled or not
+ */
+TempusTools.showExtensionEnabled = function()
+{
+    if (TempusTools.enabled) {
+        chrome.browserAction.setTitle({"title":"Disable TempusTools"});
+        chrome.browserAction.setIcon({path:"images/icon_128.png"});
+    }
+    else {
+        chrome.browserAction.setTitle({"title":"Enable TempusTools"});
+        chrome.browserAction.setIcon({path:"images/icon_greyscale_128.png"});
+    }
+};
+
+/**
+ * get options, then add a listener once i've got them
+ */
+chrome.storage.sync.get(['options', 'enabled'], function(settings) {
+    if (settings.options) {
+        if (settings.options.blacklist) {
+            var blacklist = settings.options.blacklist.replace(/\s+/, '').split(',');
+            chrome.webRequest.onBeforeRequest.addListener(
+                function() {
+                    TempusTools.addHeaders = false;
+                }, {urls: blacklist}, ['blocking']
+            );
+        }
+        if (settings.options.hasOwnProperty('maxCombinedSize')) {
+            TempusTools.maxCombinedSize = settings.options.maxCombinedSize;
+        }
+    }
+    if (settings.hasOwnProperty('enabled')) {
+        TempusTools.enabled = settings.enabled;
+        TempusTools.showExtensionEnabled();
+    }
+});
+
+/**
+ * On header send, add in the header for TempusTools and 256k limit for chrome header size
+ * note: it has to go here, not in devtools.js because devtools only gets called when you show it
+ */
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	function(details) {
+        if (TempusTools.addHeaders && TempusTools.enabled) {
+            for (var i = 0; i < details.requestHeaders.length; i++) {
+                if (details.requestHeaders[i].name == 'User-Agent') {
+                    details.requestHeaders[i].value += ' TempusTools/Debugger'; // required for old ZF and other libs (matches the regex)
+                }
+            }
+            details.requestHeaders.push({
+                    name:	'X-TempusDebugger-Version',
+                    value:	'1.0'
+                },
+                {
+                    name:   'X-Wf-Max-Combined-Size',
+                    value:  TempusTools.maxCombinedSize+''
+                });
+        }
+
+		return {
+			requestHeaders: details.requestHeaders
+		};
+	}, {urls: ["<all_urls>"]}, ['blocking', 'requestHeaders']	
+);
+
+/**
+ * Code used to inject inline.  into the current page to run
+ */
+const LOGGER = function (json, enabled) {
+    if (enabled) {
+        var commandObject = JSON.parse(unescape(json));
+        console[commandObject.type].apply(console, commandObject.params);
+    }
+};
+
+/** 
+ * messages come from devtools in the form of an object with a type of console log an a message to send
+ * all escaped and jsonified
+ */
+chrome.extension.onMessage.addListener(
+	function(commandObject) {
+		//inject LOGGER code and pass argument of our escaped stringified object
+		chrome.tabs.executeScript(null, {
+			 code: "("+ LOGGER + ")('" + commandObject + "', " + TempusTools.enabled + ");"
+		});
+	}	
+);
+
+/**
+ * Define the click handler for the badge to handle disable/enable
+ */
+chrome.browserAction.onClicked.addListener(function(tab){
+    TempusTools.enabled = !TempusTools.enabled;
+    chrome.storage.sync.set({'enabled': TempusTools.enabled}, function() {
+        TempusTools.showExtensionEnabled();
+    });
+});
---
+++ js/options.js
@@ -0,0 +1,102 @@
+/**
+ * Options Processing Script
+ *
+ * This handles saving the options and repopulating the options html page
+ *
+ * @author Aaron Saray aaron@aaronsaray.com
+ */
+
+/**
+ * Main options class
+ * @constructor
+ */
+function TempusTools_Options() {
+
+    /**
+     * Validation messages
+     */
+    var _messages = {
+        success: 'Options saved successfully.  Reloading in 5 seconds.'
+    };
+
+    /**
+     * Message types
+     */
+    var _messageTypes = {
+        success: 'success'
+    };
+
+
+    /**
+     * Default values
+     * @type {{targetVersion: string}}
+     * @private
+     */
+    var _defaultOptions = {
+        blacklist: '*://www.TheTempusProject.com/*',
+        maxCombinedSize: 261120 // 255kB
+    };
+
+    /**
+     * Initializer function: sets options, click handler
+     */
+    var _init = function() {
+        document.addEventListener('DOMContentLoaded', _restoreOptions);
+        document.querySelector('#submit').addEventListener('click', _save);
+    };
+
+    /**
+     * Used to save options to local storage
+     */
+    var _save = function() {
+        var blacklist = document.querySelector('#blacklist').value,
+            max_combined_size = document.querySelector('#max_combined_size').value;
+        var options = {
+            blacklist: blacklist,
+            maxCombinedSize: max_combined_size
+        };
+
+        chrome.storage.sync.set({'options': options}, function () {
+            _updateStatusAndReload(_messages.success, _messageTypes.success);
+        });
+    };
+
+    /**
+     * Used to update message / status
+     *
+     * Adds a transition to it too
+     * Finally reloads the extension
+     * @param message
+     * @param type
+     * @private
+     */
+    var _updateStatusAndReload = function(message, type) {
+        var status = document.querySelector('#savemessage');
+        status.innerText = message;
+        status.className = type;
+        setTimeout(function(){
+            status.className = '';
+            status.innerText = '';
+            chrome.runtime.reload(); // because options are used in background page
+        }, 5000);
+    };
+
+    /**
+     * Used to put the retrieved options back on the screen
+     * @private
+     */
+    var _restoreOptions = function() {
+        chrome.storage.sync.get('options', function(settings) {
+            if (!settings.options) {
+                settings.options = _defaultOptions;
+            }
+            document.querySelector('#blacklist').value = settings.options.blacklist;
+            document.querySelector('#max_combined_size').value = settings.options.maxCombinedSize;
+        });
+    };
+
+    // initialize the class
+    _init();
+}
+
+new TempusTools_Options();
